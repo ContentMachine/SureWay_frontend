e25d45a77202fea793ca997e330afea1f29f6bef
tobe_wdw <softwaredeveloper@wdwltd.com>
2025-01-16 13:14:12 +0100

    added the new magnets section

--- src/containers/Magnets/Magnets.tsx
+++ src/containers/Magnets/Magnets.tsx
@@ -2,11 +2,13 @@ import AppLayout from "@/layouts/AppLayout/AppLayout";
 import classes from "./Magnets.module.css";
 import SectionsHero from "@/components/SectionsHero/SectionsHero";
 import MagnetsHeaderSection from "../MagnetsHeaderSection/MagnetsHeaderSection";
+import HomeProductsSummary from "../HomeProductsSummary/HomeProductsSummary";
 
 const Magnets = () => {
   return (
     <AppLayout className={classes.container}>
       <SectionsHero title="Magnets" />
+      <HomeProductsSummary />
       <MagnetsHeaderSection />
     </AppLayout>
   );
