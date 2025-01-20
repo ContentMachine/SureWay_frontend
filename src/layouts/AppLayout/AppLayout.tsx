b2d94bfce0d06a6fcbf49372ca3f713ade40617e
tobe_wdw <softwaredeveloper@wdwltd.com>
2025-01-20 15:26:57 +0100

    fixed the build issue

--- src/layouts/AppLayout/AppLayout.tsx
+++ src/layouts/AppLayout/AppLayout.tsx
@@ -3,10 +3,6 @@
 import Header from "@/containers/Header/Header";
 import classes from "./AppLayout.module.css";
 import Footer from "@/containers/Footer/Footer";
-import { useContext } from "react";
-import { AppContext } from "@/context/AppContext";
-import Modal from "@/components/Modal/Modal";
-import InqiryModalBody from "@/containers/InqiryModalBody/InqiryModalBody";
 
 type AppLayoutTypes = {
   children: React.ReactNode;
@@ -15,20 +11,12 @@ type AppLayoutTypes = {
 };
 
 const AppLayout = ({ children, className, isDynamic }: AppLayoutTypes) => {
-  // COntext
-  const { modals, setModals } = useContext(AppContext);
   return (
-    <>
-      {modals?.enquiry && (
-        <Modal setState={setModals} body={<InqiryModalBody />} />
-      )}
-
-      <main className={`${classes.container}`}>
-        <Header isDynamic={isDynamic} />
-        <section className={className}>{children}</section>
-        <Footer />
-      </main>
-    </>
+    <main className={`${classes.container}`}>
+      <Header isDynamic={isDynamic} />
+      <section className={className}>{children}</section>
+      <Footer />
+    </main>
   );
 };
 
