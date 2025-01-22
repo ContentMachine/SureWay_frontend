86c234e0d90f38338f0f66c4f1e5fb1c1bb9563e
tobe_wdw <softwaredeveloper@wdwltd.com>
2025-01-22 17:16:27 +0100

    used suspense boundary on /magnets(2)

--- src/containers/MagnetProducts/MagnetProducts.tsx
+++ src/containers/MagnetProducts/MagnetProducts.tsx
@@ -1,22 +1,25 @@
 "use client";
 
-import React from "react";
+import React, { Suspense } from "react";
 import classes from "./MagnetProducts.module.css";
 import HomeProducts from "../HomeProducts/HomeProducts";
+import Loader from "@/components/Loader/Loader";
 
 const MagnetProducts = () => {
   return (
-    <section className={classes.container} id="custom-magnets">
-      <h4>Our Custom Magnets</h4>
-      <p>
-        Whether you're a DIY enthusiast, a professional, or simply looking to
-        add a touch of functionality to your space, our collection ensures
-        reliability and style. Browse our selection today and experience the
-        magic of magnets!
-      </p>
+    <Suspense fallback={<Loader />}>
+      <section className={classes.container} id="custom-magnets">
+        <h4>Our Custom Magnets</h4>
+        <p>
+          Whether you're a DIY enthusiast, a professional, or simply looking to
+          add a touch of functionality to your space, our collection ensures
+          reliability and style. Browse our selection today and experience the
+          magic of magnets!
+        </p>
 
-      <HomeProducts />
-    </section>
+        <HomeProducts />
+      </section>
+    </Suspense>
   );
 };
 
