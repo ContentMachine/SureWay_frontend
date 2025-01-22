20999c8de43db8c939b33613844f59256800fc0b
tobe_wdw <softwaredeveloper@wdwltd.com>
2025-01-22 17:06:08 +0100

    added the new way of displaying magnets

--- src/containers/MagnetProducts/MagnetProducts.tsx
+++ src/containers/MagnetProducts/MagnetProducts.tsx
@@ -6,54 +6,13 @@ import ProductCard from "@/components/ProductCard/ProductCard";
 import { useRouter } from "next/navigation";
 import ProductsListing from "@/components/ProductsListing/ProductsListing";
 import { valentinesProducts } from "@/utilities/products";
+import useUpdateSearchParams from "@/hooks/useUpdateSearchParams";
+import HomeProducts from "../HomeProducts/HomeProducts";
 
 const MagnetProducts = () => {
-  // States
-  const [navItems, setNavItems] = useState<navItemTypes[]>([
-    {
-      title: "Valentine's Day Magnets",
-      isActive: true,
-      description:
-        "Valentine's Day is the perfect moment to show your loved ones how much they mean to you.",
-    },
-    {
-      title: "Birthday Magnets",
-      isActive: false,
-      description:
-        "Our birthday magnet designs are perfect as personalised gifts for the birthday star. Easily customise them with your favorite photos and create a keepsake they'll treasure forever.",
-    },
-    {
-      title: "Mother's Day",
-      isActive: false,
-      description:
-        "Our birthday magnet designs are perfect as personalised gifts for the birthday star. Easily customise them with your favorite photos and create a keepsake they'll treasure forever.",
-    },
-    {
-      title: "Father's Day",
-      isActive: false,
-      description:
-        "Our birthday magnet designs are perfect as personalised gifts for the birthday star. Easily customise them with your favorite photos and create a keepsake they'll treasure forever.",
-    },
-    {
-      title: "Other Occasions",
-      isActive: false,
-      description:
-        "Our other occasion magnet designs are perfect as personalised gifts for the birthday star. Easily customise them with your favorite photos and create a keepsake they'll treasure forever.",
-    },
-  ]);
-
-  // Router
-  const router = useRouter();
-
-  //   Memo
-  const activeNav = useMemo(
-    () => navItems?.find((data) => data?.isActive),
-    [navItems]
-  );
-
   return (
     <section className={classes.container} id="custom-magnets">
-      <h4>Our Magnetic Products</h4>
+      <h4>Our Custom Magnets</h4>
       <p>
         Whether you're a DIY enthusiast, a professional, or simply looking to
         add a touch of functionality to your space, our collection ensures
@@ -61,8 +20,7 @@ const MagnetProducts = () => {
         magic of magnets!
       </p>
 
-      <SectionsNav navItems={navItems} setNavItems={setNavItems} />
-      <ProductsListing products={valentinesProducts} />
+      <HomeProducts />
     </section>
   );
 };
