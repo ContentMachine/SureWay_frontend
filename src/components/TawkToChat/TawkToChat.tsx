4901d46a2c845e6ea913d37a5f5edd6041d9d867
tobe_wdw <softwaredeveloper@wdwltd.com>
2025-02-03 10:09:11 +0100

    Added the new tawk.to url

--- src/components/TawkToChat/TawkToChat.tsx
+++ src/components/TawkToChat/TawkToChat.tsx
@@ -5,7 +5,7 @@ import { useEffect } from "react";
 const TawkToChat = () => {
   useEffect(() => {
     const script = document.createElement("script");
-    script.src = "https://embed.tawk.to/679a2978825083258e0cffe6/1iip340b1";
+    script.src = process?.env?.NEXT_PUBLIC_TAWK_WIDGET_URL as string;
     script.async = true;
     script.charset = "UTF-8";
     script.setAttribute("crossorigin", "*");
